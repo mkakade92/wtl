4b2b04018fae12229859b1854f52ce3ca1419aae
Maitraya Kakade <47203424+mkakade92@users.noreply.github.com>
2020-01-15 17:34:53 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -9,4 +9,22 @@ function validateUname(){
         alert('Invalid Username .Ensure Username does not begin with number');
         document.getElementById('uname').focus();   
      }
+
+}
+
+function validatePasswd()
+{
+    var paswd=document.getElementById('pass').value
+
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+    if(paswd.match(strongRegex)!=null)
+    {
+        alert("Strong password");
+    }
+    else
+    {
+        alert('weak password');
+    }
+
 }
